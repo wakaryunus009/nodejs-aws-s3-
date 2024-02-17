1186f5fb28a741fd79ccb3d16e848f6e834722ec
WAKAR YUNUS <82135292+wakaryunus009@users.noreply.github.com>
2024-02-18 02:55:16 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,12 +4,12 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "production",
   PORT: process.env.PORT || 3000,
   AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY || 
-  'AKIA2RXPHIJLTT6NT6ME',
+  '',
   AWS_SECRET_KEY:
-    process.env.AWS_SECRET_KEY || "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",
+    process.env.AWS_SECRET_KEY || "",
   AWS_SECRET_ACCESS_KEY:
     process.env.AWS_SECRET_ACCESS_KEY ||
-    "xzJqgjmpu3t/ltbfKN1240Id+8u7+f9oYvc2xA2e",
-  AWS_REGION: process.env.AWS_REGION || "ap-south-1",
-  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || "cmpressvideo",
+    "",
+  AWS_REGION: process.env.AWS_REGION || "",
+  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || "",
 };
