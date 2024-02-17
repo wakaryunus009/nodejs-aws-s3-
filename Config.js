1bbba009032b532fc82184c9df98ffe349696cfd
wakaryunus009 <wakaryunus009@gamil.com>
2024-02-18 02:52:17 +0530

    need to fix it with aws

--- Config.js
+++ Config.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'production',
+  NODE_ENV: process.env.NODE_ENV || "production",
   PORT: process.env.PORT || 3000,
-  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY || 'AKIASFNTDWL6XMHUYEMS',
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY || 
+  'AKIA2RXPHIJLTT6NT6ME',
   AWS_SECRET_KEY:
-    process.env.AWS_SECRET_KEY || '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',
-  AWS_REGION: process.env.AWS_REGION || 'ap-southeast-1',
-  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || 'imgandvideodemo',
+    process.env.AWS_SECRET_KEY || "xzJqgjmpu3t/ltbfKN1240Id+8u7+f9oYvc2xA2e",
+  AWS_SECRET_ACCESS_KEY:
+    process.env.AWS_SECRET_ACCESS_KEY ||
+    "xzJqgjmpu3t/ltbfKN1240Id+8u7+f9oYvc2xA2e",
+  AWS_REGION: process.env.AWS_REGION || "ap-south-1",
+  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || "cmpressvideo",
 };
